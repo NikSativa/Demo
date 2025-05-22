fce37c8235b4a999d5050a78ef463518abdd2e84
Mykyta Konopelko <nik.sativa@gmail.com>
2025-05-23 01:14:58 +0300

    upd retains

--- DI/Assemblies/ViewModelsAssembly.swift
+++ DI/Assemblies/ViewModelsAssembly.swift
@@ -4,11 +4,11 @@ import Foundation
 @MainActor
 final class ViewModelsAssembly: Assembly {
     func assemble(with registrator: Registrator) {
-        registrator.register(MainView.ViewModel.self, options: .container) { resolver in
+        registrator.register(MainView.ViewModel.self, options: .transient) { resolver in
             return .init(dataBase: resolver.resolve())
         }
 
-        registrator.register(TaskView.ViewModel.self, options: .container) { resolver, args in
+        registrator.register(TaskView.ViewModel.self, options: .transient) { resolver, args in
             return .init(dataBase: resolver.resolve(),
                          alertPresenter: resolver.resolve(),
                          task: args.first(),
